f860ab3eb60016255c057d2b6c3a50acdf5ef59a
Siddharth Tayal <125152950+Siddharth-Tayal@users.noreply.github.com>
2025-02-18 15:57:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const folders = ["models", "controllers", "routes", "utils", "middlewares", "config"];
+const files = ["server.js"];
+
+// Create folders if they don't exist
+folders.forEach(folder => {
+    const folderPath = path.join(__dirname, folder);
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath);
+        console.log(`Created folder: ${folder}`);
+    }
+});
+
+// Create files if they don't exist
+files.forEach(file => {
+    const filePath = path.join(__dirname, file);
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, "// Entry point of the application\n", "utf8");
+        console.log(`Created file: ${file}`);
+    }
+});
+
+console.log("Folder structure setup complete!");
